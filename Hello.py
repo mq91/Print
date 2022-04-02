9267e8ac18fa616d5204e3e20a1734cc136fffff
mohammed abdullah <maqarni@hotmail.com>
2022-04-02 04:11:14 +0300

    add loop

--- Hello.py
+++ Hello.py
@@ -1,1 +1,8 @@
-print("Hello")
+# Print the below statement 3 times
+for number in range(3) :  
+    print("-------------------------------------------")
+    print("I am outer loop iteration "+str(number))
+    # Inner loop
+    for another_number in range(5):  
+        print("****************************")
+        print("I am inner loop iteration "+str(another_number))
